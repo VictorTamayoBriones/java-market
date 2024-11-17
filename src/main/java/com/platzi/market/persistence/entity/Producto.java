217dfb151d13ab63e7b26c9836edbf9f23b4b17e
Víctor Manuel Tamayo Briones <victor.tamayo@circulodecredito.com.mx>
2024-11-16 20:11:24 -0600

    entitys simples

---
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -0,0 +1,95 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "productos")
+/* Usamos la anotación @Table para decirle a java que la clase
+Producto hace referencia a la tabla productos */
+public class Producto {
+    /*
+        @Id:    idProducto al ser una clave primaria simple necesita la anotación @Id para que java
+                pueda reconocer a idProducto como llave primaria.
+        @GeneratedValue:    Genera el valor automatico de la clave primaria siguiendo la secuencia
+        @Table: Usamos la anotación @Colum para indicar el nombre de la columna de nuestar tabla
+                En este caso en la clase de java tenemos idProducto, pero en la tabla tenemos id_producto
+                por medio de @Column nosotros podemos mapear correctamente las propiedades.
+    */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    private Boolean estado;
+
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
