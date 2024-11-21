48e4ee15b801eb28c1b922d1bb0096ac0f5e720c
Víctor Manuel Tamayo Briones <victor.tamayo@circulodecredito.com.mx>
2024-11-20 23:44:30 -0600

    query methods

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -4,6 +4,7 @@ import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
 
@@ -13,6 +14,14 @@ public class ProductRepository {
         return (List<Producto>) productoCrudRepository.findAll();
         //findAll() regresa un iterable, por lo que
         // debemos castear el valor a una lista de productos
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return  productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidadStock){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, true);
     }
 
 }
--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -3,8 +3,15 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 // CrudRepository recibe dos parametros:
 // 1.- La clase a la que vamos a controlar. 2.- el tipo de la clave primaria de la clase a controlar
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 
 }
