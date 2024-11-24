5ae806ae395cac24c2f1a03023e9edf1597bd330
Víctor Manuel Tamayo Briones <victor.tamayo@circulodecredito.com.mx>
2024-11-23 23:26:34 -0600

    change spring version. add methods and repository anotation

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -2,10 +2,13 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository()
 public class ProductRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -24,4 +27,15 @@ public class ProductRepository {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, true);
     }
 
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
 }
